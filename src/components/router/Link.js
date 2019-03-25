f9e87768a1e9aaac37d2fd6842c935258246b23a
Renan Gurgel <gurgel.renan@gmail.com>
2019-03-25 17:48:26 -0300

    [MASTER] Finish lesson 16

---
+++ src/components/router/Link.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types';
+
+export class Link extends Component {
+  handleClick = (evt) => {
+    evt.preventDefault()
+    window.history.pushState(null, '', this.props.to)
+  }
+
+  render() {
+    return <a href="#" onClick={this.handleClick}>{this.props.children}</a>
+  }
+}
+
+TodoForm.propTypes = {
+  to: PropTypes.string.isRequired
+}
+
