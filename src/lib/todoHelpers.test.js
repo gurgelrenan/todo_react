e031d62d19c21236c0801e036209e8648d50c5c2
Renan Gurgel <gurgel.renan@gmail.com>
2019-03-25 16:46:16 -0300

    [MASTER] Finish lesson 15

--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo, findById, toggleTodo, updateTodo} from './todoHelpers'
+import {addTodo, findById, toggleTodo, updateTodo, removeTodo} from './todoHelpers'
 import { toBuffer } from 'ip';
 
 test('addTodo should add the passed todo to the list', () => {
@@ -92,6 +92,36 @@ test('updateTodo should not mutate the original array', () => {
   const updatedTodo = { id: 2, name: 'two', isComplete: true }
 
   const result = updateTodo(startTodos, updatedTodo)
+
+  expect(result).not.toBe(startTodos)
+})
+
+test('removeTodo should remove an item by id', () => {
+  const startTodos = [
+    { id: 1, name: 'one', isComplete: false },
+    { id: 2, name: 'two', isComplete: false },
+    { id: 3, name: 'three', isComplete: false }
+  ]
+  const targetId = 2
+  const expectedTodos = [
+    { id: 1, name: 'one', isComplete: false },
+    { id: 3, name: 'three', isComplete: false }
+  ]
+
+  const result = removeTodo(startTodos, targetId)
+
+  expect(result).toEqual(expectedTodos)
+})
+
+test('removeTodo should not mutate the original array', () => {
+  const startTodos = [
+    { id: 1, name: 'one', isComplete: false },
+    { id: 2, name: 'two', isComplete: false },
+    { id: 3, name: 'three', isComplete: false }
+  ]
+  const targetId = 2
+
+  const result = removeTodo(startTodos, targetId)
 
   expect(result).not.toBe(startTodos)
 })
